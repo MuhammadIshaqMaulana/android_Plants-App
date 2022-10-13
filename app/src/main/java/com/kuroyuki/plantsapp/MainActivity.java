06aa210babc0e691bca9dc64ccdb2421477ee15a
fujikaze20 <109487411+fujikaze20@users.noreply.github.com>
2022-10-13 18:39:19 +0700

    adding file

---
+++ app/src/main/java/com/kuroyuki/plantsapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.kuroyuki.plantsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnlogin, btnsignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnlogin = findViewById(R.id.btn_login0);
+        btnsignup = findViewById(R.id.btn_signup);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://plnts.com"));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
